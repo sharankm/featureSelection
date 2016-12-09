87825a7beefbb559a9221803f07061571a67e9f1
Sharan Kumar <sharan.blore@gmail.com>
2016-12-08 23:12:12 -0800

    Add files via upload

---
+++ RowInstance.java
@@ -0,0 +1,13 @@
+package com.ai;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RowInstance {
+    private List<Double> featureList = new ArrayList<>();
+
+    public List<Double> getFeatureList() {
+        return featureList;
+    }
+}
